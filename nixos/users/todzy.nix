ee25ebd09ea3fb7efe4b023e1b4cec080937faed
Nathan Todzy <todzy@cs.wisc.edu>
2026-01-28 15:19:13 -0600

    oops rclone secret leak. (revoked)

--- nixos/users/todzy.nix
+++ nixos/users/todzy.nix
@@ -21,7 +21,7 @@
     ];
 
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILzj8rohCyv/bvtAnEHyth5OqvCje/ZkT6SVzh97XcNO todzy@todzy-ltp" # work-ltp
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHAN3AGyedkxDF71+GYFF831xVudRmoBDcJDOCFTbED todzy@todzy-ltp" # work-ltp
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJrqPmNqIsGXvKnTch3Uam3VtwG15rELb7ZyGxf5hFT6 todzy@todzydesktop" # desktop
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEP2o+gpWhwzVCCB9YB5dvmnOME9GKUvOGsZzzQPZzzV skygu@NT-Laptop" # windows laptop
 	    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMpzv5JSCEKFdI3t8Wj6yvD8Ox9Ie9aF4qBJCnUQsoFp todzy@todzyexp2" # todzyexp2.cs.wisc.edu
